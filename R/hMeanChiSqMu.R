57d74a82ba32991131943b515c2f5851d4610c94
Felix Hofmann <felix_hofmann_13@hotmail.com>
2023-06-06 17:27:27 +0200

    Remodel package and add functions

--- R/hMeanChiSqMu.R
+++ R/hMeanChiSqMu.R
@@ -14,7 +14,8 @@
 #' based on study-specific estimates and standard errors.
 #' @export
 hMeanChiSqMu <- function(
-    thetahat, se, 
+    thetahat,
+    se,
     w = rep(1, length(thetahat)),
     mu = 0,
     phi = NULL,
@@ -27,72 +28,29 @@ hMeanChiSqMu <- function(
 
   # Check inputs
   if (check_inputs) {
-    stopifnot(
-      is.numeric(thetahat),
-      length(thetahat) > 0L,
-      is.finite(thetahat),
-      
-      is.numeric(se),
-      is.finite(se),
-      min(se) > 0,
-      
-      is.numeric(w),
-      length(w) == length(thetahat),
-      is.finite(w),
-      min(w) > 0,
-      
-      is.numeric(mu),
-      length(mu) > 0L,
-      is.finite(mu),
-      
-      !is.null(alternative),
-      length(alternative) == 1L,
-      alternative %in% c("greater", "less", "two.sided", "none"),
-      
-      !is.null(distr),
-      length(distr) == 1L,
-      
-      !is.null(heterogeneity),
-      length(heterogeneity) == 1L
+    check_inputs_p_value(
+      thetahat = thetahat,
+      se = se,
+      mu = mu,
+      heterogeneity = heterogeneity,
+      alternative = alternative,
+      phi = phi,
+      tau2 = tau2
     )
-    
-    if (!length(se) %in% c(1L, length(thetahat)))
-      stop("Length of argument 'se' must be either 1 or length(thetahat).")
-    if (!is.null(phi) && length(phi) > 1L)
-      stop("Argument 'phi' must be of length 1.")
-    if (!is.null(tau2) && length(tau2) > 1L)
-      stop("Argument 'tau2' must be of length 1.")
-    if (is.null(phi) && heterogeneity == "multiplicative")
-      stop("If heterogeneity = 'multiplicative', phi must be provided.")
-    if (is.null(tau2) && heterogeneity == "additive")
-      stop("If heterogeneity = 'additive', tau2 must be provided.")
-    if (!distr %in% c("f", "chisq"))
-      stop("Argument 'distr' must be one of c('f', 'chisq').")
-    if (!is.null(phi) && length(phi) != 1L)
-      stop("Argument 'phi' must be of length 1.")
-    if (!is.null(tau2) && length(tau2) != 1L)
-      stop("Argument 'tau2' must be of length 1.")
-    if (!is.null(phi) && heterogeneity == "additive")
-      warning("If heterogeneity = 'additive', argument 'phi' is ignored.")
-    if (!is.null(tau2) && heterogeneity == "multiplicative")
-      warning(
-        "If heterogeneity = 'multiplicative', argument 'tau2' is ignored."
-      )
-    if (heterogeneity == "none" && (!is.null(phi) || !is.null(tau2)))
-      warning(
-        "If heterogeneity = 'none', arguments 'tau2' and 'phi' are ignored."
-      )
-    if (!is.null(phi) && (!is.finite(phi) || phi < 0))
-      stop("Argument 'phi' must be finite and larger than 0.")
+    check_distr_p_value(distr = distr)
+    check_w_p_value(w = w, thetahat = thetahat)
   }
   
   # match arguments
   if (length(se) == 1L) se <- rep(se, length(thetahat))
   
-  # add heterogeneity
-  if (heterogeneity == "additive") denominator <- sqrt(se^2 + tau2)
-  if (heterogeneity == "multiplicative") denominator <- sqrt(se^2 * phi)
-  if (heterogeneity == "none") denominator <- se
+  # adjust se based on heterogeneity model
+  se <- adjust_se(
+    se = se,
+    heterogeneity = heterogeneity,
+    phi = phi,
+    tau2 = tau2
+  )
   
   # store lengths of input vector
   n <- length(thetahat)
@@ -102,8 +60,8 @@ hMeanChiSqMu <- function(
   if (alternative != "none") {
     sw <- sum(sqrt(w))^2
     z <- vapply(
-      seq_along(mu),
-      function(i) (thetahat - mu[i]) / denominator,
+      mu,
+      function(mu) (thetahat - mu) / se,
       double(length(thetahat))
     )
     if (!is.matrix(z)) {
@@ -147,10 +105,14 @@ hMeanChiSqMu <- function(
   # Case when alternative is "none"
   } else if (alternative == "none") {
     sw <- sum(sqrt(w))^2
-    zH2 <- vapply(seq_along(mu), function(i){
-      z <- (thetahat - mu[i]) / denominator
-      sw / sum(w / z^2)
-    }, double(1L))
+    zH2 <- vapply(
+      mu,
+      function(mu){
+        z <- (thetahat - mu) / se
+        sw / sum(w / z^2)
+      },
+      double(1L)
+    )
     res <- if (distr == "chisq") {
       stats::pchisq(q = zH2, df = 1, lower.tail = FALSE)
     } else if (distr == "f") {
--- R/kTRMu.R
+++ R/kTRMu.R
@@ -1,5 +1,7 @@
 #' Implementation of the "k-Trials" rule.
-#'
+#' @details
+#' The function is vectorized over the argument \code{mu}.
+#' 
 #' @template thetahat
 #' @template se
 #' @template mu
@@ -14,27 +16,31 @@
 #' @examples
 #' thetahat <- c(0.38041824, -0.22170681, -0.09155859)
 #' se <- c(0.2256202, 0.2432796, 0.1051743)
-#' phi <- estimatePhi(thetahat = thetahat, se=se)
+#' phi <- estimatePhi(thetahat = thetahat, se = se)
 #' tau2 <- estimateTau2(
-#'   thetahat,
-#'   se,
-#'   control = list(stepadj = 0.5, maxiter = 1000, threshold = 1e-6)
+#'     thetahat,
+#'     se,
+#'     control = list(stepadj = 0.5, maxiter = 1000, threshold = 1e-6)
 #' )
-#' mymu <- seq(min(thetahat - 3 * se), max(thetahat + 3 * se), length.out=1000)
+#' mymu <- seq(
+#'     min(thetahat - 3 * se),
+#'     max(thetahat + 3 * se),
+#'     length.out = 1000
+#' )
 #' p_add <- kTRMu(
-#'   thetahat, se,
-#'   mymu,
-#'   tau2 = tau2,
-#'   phi = phi,
-#'   heterogeneity = "additive"
+#'     thetahat, se,
+#'     mymu,
+#'     tau2 = tau2,
+#'     phi = phi,
+#'     heterogeneity = "additive"
 #' )
 #' p_mult <- kTRMu(
-#'   thetahat,
-#'   se,
-#'   mymu,
-#'   tau2 = tau2,
-#'   phi = phi,
-#'   heterogeneity = "multiplicative"
+#'     thetahat,
+#'     se,
+#'     mymu,
+#'     tau2 = tau2,
+#'     phi = phi,
+#'     heterogeneity = "multiplicative"
 #' )
 kTRMu <- function(
   thetahat,
@@ -47,26 +53,40 @@ kTRMu <- function(
 ) {
 
   if (check_inputs) {
-    if (is.null(tau2) && heterogeneity == "additive")
-      stop("If heterogeneity = 'additive', tau2 must be provided")
-    if (is.null(phi) && heterogeneity == "multiplicative")
-      stop("If heterogeneity = 'multiplicative', phi must be provided")
-    if (heterogeneity == "none" && (!is.null(tau2) || !is.null(phi)))
-      warning(
-        paste0("Ignoring parameter(s) phi and tau2 as heterogeneity = 'none'")
-    )
+      check_inputs_p_value(
+        thetahat = thetahat,
+        se = se,
+        mu = mu,
+        heterogeneity = heterogeneity,
+        alternative = alternative,
+        phi = phi,
+        tau2 = tau2
+      )
   }
-
-  if (heterogeneity == "additive")
-    se <- sqrt(se^2 + tau2)
-  if (heterogeneity == "multiplicative")
-    se <- se * sqrt(phi)
-  z <- vapply(mu, function(mu) (thetahat - mu) / se, double(length(thetahat)))
-  if (is.null(dim(z)))
-    dim(z) <- c(1, length(z))
-  p <- ReplicationSuccess::z2p(z, alternative = "two.sided")
-  dim(p) <- dim(z)
-  pmax <- apply(p, 2, max)
-  k <- length(thetahat)
-  return(pmax^k)
+  
+  # recycle `se` if needed
+  if (length(se) == 1L) se <- rep(se, length(thetahat))
+  
+  # adjust se based on heterogeneity model
+  se <- adjust_se(
+    se = se,
+    heterogeneity = heterogeneity,
+    phi = phi,
+    tau2 = tau2
+  )
+  
+  # Get lengths
+  n <- length(thetahat)
+  
+  if(alternative == "none") {
+    z <- vapply(mu, function(mu) (thetahat - mu) / se, double(length(thetahat)))
+    if (is.null(dim(z)))
+      dim(z) <- c(1L, length(z))
+    p <- 2 * stats::pnorm(abs(z), lower.tail = FALSE) # ReplicationSuccess::z2p
+    pmax <- apply(p, 2L, max)
+  } else {
+    stop("Invalid argument 'alternative'.")
+  }
+  
+  return(pmax^n)
 }
