7178887b950df582c3b95edeb3f5071c7bf42192
Felix Hofmann <felix_hofmann_13@hotmail.com>
2023-06-25 12:30:49 +0200

    Make test function available everywhere

--- tests/testthat/globals.R
+++ tests/testthat/globals.R
@@ -1,18 +1,27 @@
+path <- "https://raw.githubusercontent.com/felix-hof/hMean/main/R/kTRMu.R"
+fun_name <- "kTRMu"
+
 # get the old function
-get_old_FUN <- function(path, fun_name) {
-  # Source the utils file as well
-  source(
-    "https://raw.githubusercontent.com/felix-hof/hMean/main/R/utils.R",
-    local = TRUE
-  )
-  new_name <- paste0(fun_name, "_old")
-  def <- readLines(path)
-  comments <- grepl("^\\s*#", def)
-  def <- def[!comments]
-  def <- paste0(def, collapse = "\n")
-  regex <- paste0(fun_name, "\\s*<-\\s*function")
-  replace <- paste0(new_name, " <- function")
-  def <- sub(regex, replace, def)
-  eval(parse(text = def))
-  get(new_name)
+get_old_FUN <- function(
+    path,
+    fun_name
+) {
+
+    # Source the utils file as well
+    code_utils <- paste0(
+        readLines(
+            "https://raw.githubusercontent.com/felix-hof/hMean/main/R/utils.R",
+            warn = FALSE
+        ),
+        collapse = "\n"
+    )
+    code_function <- paste0(
+        readLines(path, warn = FALSE),
+        collapse = "\n"
+    )
+    e <- new.env()
+    eval(code_utils, envir = e)
+    eval(code_function, envir = e)
+
+    get(fun_name, envir = e)
 }
