f8350a31834d5c327e502f6cf3ef473b95b6e8a0
Felix Hofmann <felix_hofmann_13@hotmail.com>
2023-06-30 17:25:05 +0200

    Edit app

--- showcase/app.R
+++ showcase/app.R
@@ -8,16 +8,6 @@ make_plot <- function(pars) {
     with(
         pars,
         {
-            phi <- if (het == "multiplicative") {
-                estimatePhi(thetahat, se)
-            } else {
-                NULL
-            }
-            tau2 <- if (het == "additive") {
-                estimateTau2(thetahat, se)
-            } else {
-                NULL
-            }
             pval <- ggPvalueFunction(
                 thetahat = thetahat,
                 se = se,
@@ -48,10 +38,16 @@ ui <- fluidPage(
     # Inputs
     fluidRow(
         column(
-            6,
+            12,
             selectInput(
                 "fct1", "P-value function(s)",
-                choices = c("hMean", "k-Trials", "Pearson", "Edgington", "Fisher"),
+                choices = c(
+                    "hMean",
+                    "k-Trials",
+                    "Pearson",
+                    "Edgington",
+                    "Fisher"
+                ),
                 multiple = TRUE,
                 selected = "hMean"
             ),
@@ -82,47 +78,53 @@ ui <- fluidPage(
             numericInput(
                 "level1", "Confidence level",
                 value = 0.95, min = 0.001, max = 0.999, step = 0.001
-            )
-
+            ),
+            sliderInput("xlim1", "Range of x-axis", value = c(-3, 3))
         ),
-        column(
-            6,
-            selectInput(
-                "fct2", "P-value function(s)",
-                choices = c("hMean", "k-Trials", "Pearson", "Edgington", "Fisher"),
-                multiple = TRUE,
-                selected = "hMean"
-
-            ),
-            conditionalPanel(
-                'input.fct2.includes("hMean")',
-                selectInput(
-                    "distr2", "Harmonic Mean distribution",
-                    choices = c("chisq", "f"),
-                    multiple = TRUE,
-                    selected = "chisq"
-                )
-            ),
-            numericInput(
-                "n2", "Number of studies", value = 3, min = 2, max = 50,
-                step = 1
-            ),
-            numericInput("seed2", "RNG seed", value = 42),
-            numericInput("mean2", "Effect: Mean", value = 0, step = 0.1),
-            numericInput("sd2", "Effect: SD", value = 1, step = 0.1),
-            numericInput("shape2", "SE: shape", value = 5, step = 0.1),
-            numericInput("rate2", "SE: rate", value = 5, step = 0.1),
-            selectInput(
-                "het2", "Heterogeneity",
-                choices = c("none", "additive", "multiplicative"),
-                selected = "none",
-                multiple = TRUE
-            ),
-            numericInput(
-                "level2", "Confidence level",
-                value = 0.95, min = 0.001, max = 0.999, step = 0.001
-            )
-        )
+        # column(
+        #     6,
+        #     selectInput(
+        #         "fct2", "P-value function(s)",
+        #         choices = c(
+        #             "hMean",
+        #             "k-Trials",
+        #             "Pearson",
+        #             "Edgington",
+        #             "Fisher"
+        #         ),
+        #         multiple = TRUE,
+        #         selected = "hMean"
+        #
+        #     ),
+        #     conditionalPanel(
+        #         'input.fct2.includes("hMean")',
+        #         selectInput(
+        #             "distr2", "Harmonic Mean distribution",
+        #             choices = c("chisq", "f"),
+        #             multiple = TRUE,
+        #             selected = "chisq"
+        #         )
+        #     ),
+        #     numericInput(
+        #         "n2", "Number of studies", value = 3, min = 2, max = 50,
+        #         step = 1
+        #     ),
+        #     numericInput("seed2", "RNG seed", value = 42),
+        #     numericInput("mean2", "Effect: Mean", value = 0, step = 0.1),
+        #     numericInput("sd2", "Effect: SD", value = 1, step = 0.1),
+        #     numericInput("shape2", "SE: shape", value = 5, step = 0.1),
+        #     numericInput("rate2", "SE: rate", value = 5, step = 0.1),
+        #     selectInput(
+        #         "het2", "Heterogeneity",
+        #         choices = c("none", "additive", "multiplicative"),
+        #         selected = "none",
+        #         multiple = TRUE
+        #     ),
+        #     numericInput(
+        #         "level2", "Confidence level",
+        #         value = 0.95, min = 0.001, max = 0.999, step = 0.001
+        #     )
+        # )
     ),
 
     # Action Button
@@ -135,13 +137,13 @@ ui <- fluidPage(
     # Show outputs
     fluidRow(
         column(
-            6,
+            12,
             plotOutput("plot1")
-        ),
-        column(
-            6,
-            plotOutput("plot2")
-        )
+        )#,
+        # column(
+        #     6,
+        #     plotOutput("plot2")
+        # )
     )
 )
 
@@ -171,52 +173,53 @@ server <- function(input, output, session) {
                 het = het,
                 pValueFUN = pValueFUN,
                 level = input$level1,
-                distr = distr
+                distr = distr,
+                xlim = input$xlim1
             )
         }
     )
 
-    pars2 <- eventReactive(
-        input$go,
-        {
-            set.seed(input$seed2)
-            thetahat <- rnorm(
-                n = input$n2,
-                mean = input$mean2,
-                sd = input$sd2
-            )
-            se <- rgamma(
-                n = input$n2,
-                shape = input$shape2,
-                rate = input$rate2
-            )
-            het <- input$het2
-            pValueFUN <- input$fct2
-            d <- input$distr2
-            distr <- if (!is.null(d)) "chisq" else d
-            list(
-                thetahat = thetahat,
-                se = se,
-                het = het,
-                pValueFUN = pValueFUN,
-                level = input$level2,
-                distr = distr
-            )
-        }
-    )
+    # pars2 <- eventReactive(
+    #     input$go,
+    #     {
+    #         set.seed(input$seed2)
+    #         thetahat <- rnorm(
+    #             n = input$n2,
+    #             mean = input$mean2,
+    #             sd = input$sd2
+    #         )
+    #         se <- rgamma(
+    #             n = input$n2,
+    #             shape = input$shape2,
+    #             rate = input$rate2
+    #         )
+    #         het <- input$het2
+    #         pValueFUN <- input$fct2
+    #         d <- input$distr2
+    #         distr <- if (!is.null(d)) "chisq" else d
+    #         list(
+    #             thetahat = thetahat,
+    #             se = se,
+    #             het = het,
+    #             pValueFUN = pValueFUN,
+    #             level = input$level2,
+    #             distr = distr
+    #         )
+    #     }
+    # )
 
     # pars <- list(
     #     thetahat = rnorm(3),
     #     se = rgamma(3, 5, 5),
     #     level = 0.95,
-    #     het = "none",
-    #     pValueFUN = c("hMean", "Edgington", "Fisher"),
+    #     het = c("none", "additive"),
+    #     pValueFUN = c("hMean"),
     #     distr = c("chisq", "f")
     # )
     # make_plot(pars)
 
     output$plot1 <- renderPlot(make_plot(pars1()))
-    output$plot2 <- renderPlot(make_plot(pars2()))
+    # output$plot2 <- renderPlot(make_plot(pars2()))
 }
 
 shinyApp(ui, server)
